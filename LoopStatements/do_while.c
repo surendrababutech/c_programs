572351a34628bb7d3728685c884322e488297faf
Surendra Babu <surendrababu.tech@gmail.com>
2025-01-30 17:25:00 +0000

    Jan 30

---
+++ LoopStatements/do_while.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+int main()
+{
+	do{
+		printf("Executed\n");
+	}
+	while(false);
+
+	return 0;
+}
---
+++ LoopStatements/while.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+	int arr[] = {10, 15, 20, 30, 50};
+
+	int i=0;
+
+	while(i < 5)
+	{
+		printf("%d ", arr[i]);
+		i++;
+	}
+	printf("\n");
+
+	return 0;
+}
+
